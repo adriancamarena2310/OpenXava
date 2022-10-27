26a3d7147df81530a14c597c48b6e07882247381
adrian camarena <adriancamarena2310@gmail.com>
2022-10-26 20:36:06 -0500

    Cambio de nombre y  validacion avanzada 3

---
+++ facturacion/src/com/tuempresa/facturacion/calculadores/CalculadorPorcentajeIVA.java
@@ -0,0 +1,10 @@
+package com.tuempresa.facturacion.calculadores;
+
+//public class CalculadorPorcentajeIVA implements ICalculator {
+	
+	
+	//public Object calculate()throws Exception{
+		//return PreferenciasFacturacion.getProcentajeIVADefecto();
+	//}
+
+//}
